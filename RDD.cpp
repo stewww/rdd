a88226b4df02c1124eb355c179658609fdbfae2a
Steven Hwu <steven.hwu@sjsu.edu>
2016-08-28 16:06:21 -0700

    Bringup of contours which can detect vehicles

--- RDD.cpp
+++ RDD.cpp
@@ -3,42 +3,6 @@
 using namespace cv;
 using namespace std;
 
-const char * trackbars = "Config Trackbars";
-const char * minThresholdStr = "Min Threshold";
-const char * maxThresholdStr = "Max Threshold";
-const char * minAreaStr = "Min Area";
-const char * maxAreaStr = "Max Area";
-const char * minCircularityStr = "Min Circularity";
-const char * maxCircularityStr = "Max Circularity";
-const char * minConvexityStr = "Min Convexity";
-const char * maxConvexityStr = "Max Convexity";
-const char * minInertiaStr = "Min Inertia";
-const char * maxInertiaStr = "Max Inertia";
-
-/*** Raw Values ***/
-int minThresholdValue = 20;
-int maxThresholdValue = 200;
-int minAreaValue = 400;
-int maxAreaValue = 3000;
-
-/*** Percentage ***/
-int minCircularityValue = 50;
-int maxCircularityValue = 70;
-int minConvexityValue = 60;
-int maxConvexityValue = 90;
-int minInertiaValue = 30;
-int maxInertiaValue = 80;
-
-static int minThresholdMax = 500;
-static int maxThresholdMax = 500;
-static int minAreaMax = 3000;
-static int maxAreaMax = 3000;
-static int minCircularityMax = 100;
-static int maxCircularityMax = 100;
-static int minConvexityMax = 100;
-static int maxConvexityMax = 100;
-static int minInertiaMax = 100;
-static int maxInertiaMax = 100;
 
 static void trackbarCb(int, void *)
 {
@@ -49,20 +13,6 @@ int main( int argc, char** argv )
 {
 	VideoCapture capture;
 	capture.open("testvid00.avi");
-
-
-	namedWindow(trackbars, WINDOW_NORMAL);
-	createTrackbar(minThresholdStr, trackbars, &minThresholdValue, minThresholdMax, trackbarCb);
-	createTrackbar(maxThresholdStr, trackbars, &maxThresholdValue, maxThresholdMax, trackbarCb);
-	createTrackbar(minAreaStr, trackbars, &minAreaValue, minAreaMax, trackbarCb);
-	createTrackbar(maxAreaStr, trackbars, &maxAreaValue, maxAreaMax, trackbarCb);
-	createTrackbar(minCircularityStr, trackbars, &minCircularityValue, minCircularityMax, trackbarCb);
-	createTrackbar(maxCircularityStr, trackbars, &maxCircularityValue, maxCircularityMax, trackbarCb);
-	createTrackbar(minConvexityStr, trackbars, &minConvexityValue, minConvexityMax, trackbarCb);
-	createTrackbar(maxConvexityStr, trackbars, &maxConvexityValue, maxConvexityMax, trackbarCb);
-	createTrackbar(minInertiaStr, trackbars, &minInertiaValue, minInertiaMax, trackbarCb);
-	createTrackbar(maxInertiaStr, trackbars, &maxInertiaValue, maxInertiaMax, trackbarCb);
-
 
 	if (!capture.isOpened())
 	{
@@ -76,7 +26,7 @@ int main( int argc, char** argv )
 		static bool start = true;
 		bool validDiff = false;
 		static uint32_t counter = 0;
-		static uint8_t sampleInterval = 5; ///< Every sampleInterval frames we will grab one frame
+		static uint8_t sampleInterval = 3; ///< Every sampleInterval frames we will grab one frame
 
 
 		if (updateImage || start)
@@ -88,58 +38,47 @@ int main( int argc, char** argv )
 		static Mat prev_grey_image;
 		static Mat curr_grey_image;
 		static Mat diff_grey_image;
+		vector<vector<Point> > contours;
 		if (!(counter % sampleInterval))
 		{
+			Mat contours_drawing;
+			Mat threshold_diff;
 			curr_grey_image.copyTo(prev_grey_image);
 			cvtColor(im, curr_grey_image, CV_BGR2GRAY);
 			if (prev_grey_image.size().height > 0)
 			{
 				validDiff = true;
 				absdiff(prev_grey_image, curr_grey_image, diff_grey_image);
+
+
+			    vector<vector<Point> > contours;
+			    vector<Vec4i> hierarchy;
+
+			    diff_grey_image.copyTo(threshold_diff);
+			    threshold(threshold_diff, threshold_diff, 50, 200, THRESH_BINARY);
+			    threshold_diff.copyTo(contours_drawing);
+			    findContours(contours_drawing, contours, hierarchy,
+			        CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
+			    int idx = 0;
+			    for( ; idx >= 0; idx = hierarchy[idx][0] )
+			    {
+			        Scalar color( rand()&255, rand()&255, rand()&255 );
+			        drawContours( contours_drawing, contours, idx, color, CV_FILLED, 8, hierarchy );
+			    }
+
+			    if (!contours_drawing.empty())
+			    {
+			    	imshow("Threshold diff", threshold_diff);
+			    	imshow("Contours", contours_drawing);
+			    }
+
 			}
+
 		}
 
-		SimpleBlobDetector::Params params;
-		// Change thresholds
-		params.minThreshold = minThresholdValue;
-		params.maxThreshold = maxThresholdValue;
+		// Show Image
+		imshow("image", im );
 
-		// Filter by Area.
-		params.filterByArea = true;
-		params.minArea = minAreaValue;
-		params.maxArea = maxAreaValue;
-
-		// Filter by Circularity
-		params.filterByCircularity = true;
-		params.minCircularity = (float)minCircularityValue / 100.0F;
-		params.maxCircularity = (float)maxCircularityValue / 100.0F;
-
-		// Filter by Convexity
-		params.filterByConvexity = false;
-		params.minConvexity = (float)minConvexityValue / 100.0F;
-		params.maxConvexity = (float)maxConvexityValue / 100.0F;
-
-		// Filter by Inertia
-		params.filterByInertia = true;
-		params.minInertiaRatio = (float)minInertiaValue / 100.0F;
-		params.maxInertiaRatio = (float)maxInertiaValue / 100.0F;
-
-		// Set up the detector with default parameters.
-		Ptr<SimpleBlobDetector> d = SimpleBlobDetector::create(params);
-
-
-
-		// Detect blobs.
-		std::vector<KeyPoint> keypoints;
-		d->detect( im, keypoints);
-
-		// Draw detected blobs as red circles.
-		// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
-		Mat im_with_keypoints;
-		drawKeypoints( im, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-
-		// Show blobs
-		imshow("keypoints", im_with_keypoints );
 		// And differences
 		if (validDiff)
 		{
--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Whenever you drag and drop files you don't want to check in, make sure to add th
 Do not put platform specific files in this repository. Only source files (.c/.h, etc.) or git files (.gitignore)
 
 Video files should not be included (possible limit of repository size by github)
+
+Infrastructural requirements:
+ - Adding FPS on matrices (create an wrapper API for imshow that filters and calculates frame rate, can be a sliding window average)
+ - Create platform specific headers for opencv_windows and opencv_linux for any differences (or other opencv version-specific function calls)
+ - Generate test cases for tracking lanes/vehicles in videos, come up with API for this. This may have two components, a lightweight method that can be run in conjunction with the program, with simple comparisons being made during the execution, and more complex comparisons after the execution. This could include analysis tools for various steps in our algorithms
+
+Infrastructural "nice to haves" 
+ - Automate blob detection tests, maybe this will be a better approach (probably won't be)
