7170db4f17bb7b1ab5b281d3329248b403f5bbc4
James <Drummerman432@yahoo.com>
2016-09-05 15:15:18 -0700

    updating helper files with cropImage

--- helper_opencv.cpp
+++ helper_opencv.cpp
@@ -1,5 +1,23 @@
 #include "helper_opencv.hpp"
 
+void helper_cropImage(Mat * inputImage, Mat * croppedImage)
+{
+	Rect rect;
+	Point one, two;
+	Rect box;
+        
+	one.x = 200;
+	one.y = 400;
+	two.x = 1100;
+	two.y = 700;
+
+	box.width = abs(one.x - two.x);
+	box.height = abs(one.y - two.y);
+	box.x = min(one.x, two.x);
+	box.y = min(one.y, two.y);
+
+	*croppedImage = (*inputImage)(box);
+}
 
 void helper_trackbarSimple(const string trackbarName, const string windowName, int * variable, const int maxValue)
 {
